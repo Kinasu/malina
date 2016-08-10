1a841d1d3d0c4dca692d11e7630999ad94bac795
Kinasu <ashtoreet@gmail.com>
2016-08-10 12:33:17 +0400

    Add files via upload

--- app.rb
+++ app.rb
@@ -1,12 +1,44 @@
 #encoding: utf-8
-require 'rubygems'
 require 'sinatra'
+require 'rubygems'
 
 
 get '/' do
-	erb :layout			
+	erb :index
 end
 
-get '/contacts' do
-	erb :contacts
+get '/news' do
+	erb :news
+end
+
+get '/foto' do
+	erb :foto
+end
+
+get '/otziv' do
+	erb :otziv
+end
+
+get '/contacti' do
+	erb :contacti
+end
+
+get '/pravila' do
+	erb :pravila
+end
+
+get '/schedule' do
+	erb :schedule
+end
+
+get '/doemail' do
+	erb :doemail
+end
+ 
+post '/doemail/' do
+Pony.mail :to => "ashtoreet@gmail.com",
+    :from => "mggu@newmail.ru",
+    :subject => "Contact form at nepg.biz",
+    :body => erb(:doemail)
+    "Thank you! Your message been send to administator"
 end
